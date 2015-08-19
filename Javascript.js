2c6bc5879ae6cc32b65b7974f84c10081802c581
KicksTV <Kicksmedia1@gmail.com>
2015-08-19 13:39:29 +0100

    Changes

--- Javascript.js
+++ Javascript.js
@@ -2,3 +2,11 @@
 $('.handle').on('click', function(){
 $('nav ul').toggleClass('showing');
 });
+
+function changeBG(el,bg,cov,cen) {
+	var elem = document.getElementById(el);
+	elem.style.transition = "background 2.0s linear 2s"
+	elem.style.background = bg;
+	elem.style.background = cov;
+	elem.style.background = cen;
+}
